c6abb3295fd559ed256a906c0af4a09c8df26146
Denis <denis.lavrushko@infobip.com>
2021-04-26 09:30:59 +0300

    add ns servers list to route53

--- hosted-zone.tf
+++ hosted-zone.tf
@@ -1,9 +1,16 @@
 
 resource "aws_route53_zone" "main" {
   name = var.domain_name
-  name_servers = var.ns_servers_list
   tags = {
     Service     = "workadventrure"
     Environment = var.environment_name
   }
+}
+
+resource "aws_route53_record" "main-ns" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = var.domain_name
+  type    = "NS"
+  ttl     = "3600"
+  records = var.ns_servers_list
 }
